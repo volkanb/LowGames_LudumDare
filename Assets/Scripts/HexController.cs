10547967c845d8890c04a272942efbab4dd810b6
volkanb <volkanbenli92@gmail.com>
2017-12-13 11:39:47 -0500

    second commit

---
+++ Assets/Scripts/HexController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
+
+            //Converting Mouse Pos to 2D (vector2) World Pos
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+            if (hit)
+            {
+                Debug.Log("Hit " + hit.transform.gameObject.name);
+                if (hit.transform.gameObject.tag == "Hex")
+                {
+                    Debug.Log("It's working!");
+                }
+                else
+                {
+                    Debug.Log("nopz");
+                }
+            }
+            else
+            {
+                Debug.Log("No hit");
+            }
+            Debug.Log("Mouse is down");
+        }
+
+    }
+}
---
+++ Assets/Scripts/HexSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexSpawner : MonoBehaviour {
+
+    public GameObject hex;
+    public float startDelay = 2.0f;
+    public float spawnDelay = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("SpawnHex", startDelay, spawnDelay);
+
+    }
+	
+    void SpawnHex()
+    {
+        Instantiate(hex, transform.position, Quaternion.identity);
+    }
+
+}
